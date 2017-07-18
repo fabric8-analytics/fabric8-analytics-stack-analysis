89d369d1acc9019f0005994ad18f5823b5e5de9c
Tuhin Sharma <tuhinsharma@Tuhins-MacBook-Pro.local>
2017-07-18 19:36:27 +0530

    trunc_string_at is added

--- analytics_platform/kronos/src/kronos_util.py
+++ analytics_platform/kronos/src/kronos_util.py
@@ -21,8 +21,11 @@ def generate_matrix_from_pandas_df(df, node_list):
     result = np.vstack((y, np.zeros((1, len(node_list)))))
     return result
 
+
 def trunc_string_at(s, d, n1, n2):
-    """Returns s truncated at the n'th occurrence of the delimiter, d"""
+    """
+    Returns s truncated at the n'th occurrence of the delimiter, d
+    """
     if n2 > 0:
         result = d.join(s.split(d, n2)[n1:n2])
     else:
