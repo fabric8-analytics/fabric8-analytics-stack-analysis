ff87c7d4eec14dde8f815c838b061d0e63346b79
Tuhin Sharma <tuhinsharma@Tuhins-MacBook-Pro.local>
2017-07-20 18:52:09 +0530

    folder restructuring done

--- analytics_platform/kronos/softnet/scripts/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# --------------------------------------------------------------------------------------------------
-# start web service to provide rest end points for this container
-# --------------------------------------------------------------------------------------------------
-zip -r /tmp/training.zip /analytics_platform /util
-
-gunicorn --pythonpath / -b 0.0.0.0:$SERVICE_PORT -t $SERVICE_TIMEOUT rest_api:app
