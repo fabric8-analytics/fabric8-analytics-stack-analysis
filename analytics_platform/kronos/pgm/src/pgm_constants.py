0c054e514aa846b165765ba6b112a12378b0c91d
Tuhin Sharma <tuhinsharma@Tuhins-MacBook-Pro.local>
2017-08-03 20:07:25 +0530

    constants for topic_list is added

--- analytics_platform/kronos/pgm/src/pgm_constants.py
+++ analytics_platform/kronos/pgm/src/pgm_constants.py
@@ -10,6 +10,8 @@ KD_EDGE_LIST = "kronos_edge_list"
 KD_SIMILAR_PACKAGE_MAP = "similar_package_dict"
 KD_PACKAGE_NAME = "package_name"
 KD_SIMILARITY_SCORE = "similarity_score"
+KD_PACKAGE_TO_TOPIC_MAP = "package_to_topic_dict"
+KD_TOPIC_LIST = "topic_list"
 
 KD_OUTPUT_FOLDER = "data_kronos_dependency"
 COM_OUTPUT_FOLDER = "data_co_occurrence_matrix"
@@ -23,6 +25,7 @@ KRONOS_OUTLIER_PROBABILITY_THRESHOLD_VALUE = 0.9
 KRONOS_UNKNOWN_PACKAGE_RATIO_THRESHOLD_VALUE = 0.2
 KRONOS_COMPANION_PACKAGE_COUNT_THRESHOLD_VALUE = 4
 KRONOS_ALTERNATE_PACKAGE_COUNT_THRESHOLD_VALUE = 2
+KRONOS_OUTLIER_COUNT_THRESHOLD_VALUE = 2
 KRONOS_OUTPUT_FOLDER = "data_kronos_user_eco"
 KRONOS_OUTLIER_PACKAGE_NAME = "package_name"
 KRONOS_OUTLIER_PROBABILITY = "outlier_prbability"
@@ -33,6 +36,7 @@ KRONOS_ALTERNATE_PACKAGES = "alternate_packages"
 KRONOS_COMPANION_PACKAGES = "companion_packages"
 KRONOS_OUTLIER_PACKAGES = "outlier_package_list"
 KRONOS_MISSING_PACKAGES = "missing_packages"
+KRONOS_PACKAGE_TO_TOPIC_DICT = "package_to_topic_dict"
 
 """
 Constants for Input Request for scoring
@@ -44,3 +48,9 @@ KRONOS_COMPANION_PACKAGE_COUNT_THRESHOLD_NAME = "comp_package_count_threshold"
 KRONOS_ALTERNATE_PACKAGE_COUNT_THRESHOLD_NAME = "alt_package_count_threshold"
 KRONOS_OUTLIER_PROBABILITY_THRESHOLD_NAME = "outlier_probability_threshold"
 KRONOS_UNKNOWN_PACKAGE_RATIO_THRESHOLD_NAME = "unknown_packages_ratio_threshold"
+KRONOS_OUTLIER_COUNT_THRESHOLD_NAME = "outlier_package_count_threshold"
+
+"""
+Constants for Gnosis PTM
+"""
+GNOSIS_PTM_TOPIC_PREFIX = "c_"
