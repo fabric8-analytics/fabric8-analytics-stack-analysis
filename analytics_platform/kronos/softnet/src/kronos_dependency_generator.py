6db80f3bfc2f3b10a6934351c6663053266f7322
rootavish <avgupta@redhat.com>
2017-11-29 19:30:08 +0530

    Fix indirect import

--- analytics_platform/kronos/softnet/src/kronos_dependency_generator.py
+++ analytics_platform/kronos/softnet/src/kronos_dependency_generator.py
@@ -1,5 +1,11 @@
-from analytics_platform.kronos.softnet.src.softnet_util import *
+import analytics_platform.kronos.softnet.src.softnet_constants as softnet_constants
+import util.softnet_util as softnet_utils
 from util.data_store.local_filesystem import LocalFileSystem
+import daiquiri
+import logging
+
+daiquiri.setup(level=logging.INFO)
+_logger = daiquiri.getLogger(__name__)
 
 
 class KronosDependencyGenerator(object):
@@ -21,40 +27,50 @@ class KronosDependencyGenerator(object):
         :param pkg_topic_store: Package Topic graph store.
 
         :return: Object of class KronosDependencyGenerator."""
-
+        _logger.info("Started kronos dependency graph generation")
         package_list = list(package_to_topic_dict.keys())
-        component_class_list = gnosis_ref_arch_dict.get(GNOSIS_RA_COMPONENT_CLASS_LIST)
+        component_class_list = gnosis_ref_arch_dict.get(
+            softnet_constants.GNOSIS_RA_COMPONENT_CLASS_LIST)
 
         component_class_to_package_edge_list, component_class_to_package_dict = \
             cls._generate_component_class_to_package_edge_list_and_dict(
                 package_list, component_class_list, package_to_topic_dict)
 
-        gnosis_ref_arch_intent_list = gnosis_ref_arch_dict.get(GNOSIS_RA_INTENT_LIST)
+        gnosis_ref_arch_intent_list = gnosis_ref_arch_dict.get(
+            softnet_constants.GNOSIS_RA_INTENT_LIST)
         kronos_intent_list = gnosis_ref_arch_intent_list + component_class_list
         kronos_node_list = package_list + kronos_intent_list
 
-        gnosis_ref_arch_edge_list = gnosis_ref_arch_dict.get(GNOSIS_RA_EDGE_LIST)
+        gnosis_ref_arch_edge_list = gnosis_ref_arch_dict.get(
+            softnet_constants.GNOSIS_RA_EDGE_LIST)
 
         kronos_dependency_edge_list = gnosis_ref_arch_edge_list + \
             component_class_to_package_edge_list
 
-        parent_tuple_list = generate_parent_tuple_list(kronos_node_list,
-                                                       kronos_dependency_edge_list)
+        parent_tuple_list = softnet_utils.generate_parent_tuple_list(
+                                                             kronos_node_list,
+                                                             kronos_dependency_edge_list)
         parent_tuple_list_string = LocalFileSystem.convert_list_of_tuples_to_string(
             parent_tuple_list)
         similar_package_dict = cls._generate_similar_package_dict(package_to_topic_dict,
                                                                   topic_to_package_dict)
 
         kronos_dependency_dict = dict()
-        kronos_dependency_dict[KD_PACKAGE_LIST] = package_list
-        kronos_dependency_dict[KD_INTENT_LIST] = kronos_intent_list
-        kronos_dependency_dict[KD_INTENT_DEPENDENCY_MAP] = gnosis_ref_arch_dict.get(GNOSIS_RA_DICT)
-        kronos_dependency_dict[KD_COMPONENT_DEPENDENCY_MAP] = component_class_to_package_dict
-        kronos_dependency_dict[KD_PARENT_TUPLE_LIST] = parent_tuple_list_string
-        kronos_dependency_dict[KD_EDGE_LIST] = kronos_dependency_edge_list
-        kronos_dependency_dict[KD_SIMILAR_PACKAGE_MAP] = similar_package_dict
-        kronos_dependency_dict[KD_PACKAGE_TO_TOPIC_MAP] = package_to_topic_dict
-
+        kronos_dependency_dict[softnet_constants.KD_PACKAGE_LIST] = package_list
+        kronos_dependency_dict[softnet_constants.KD_INTENT_LIST] = kronos_intent_list
+        kronos_dependency_dict[
+            softnet_constants.KD_INTENT_DEPENDENCY_MAP] = gnosis_ref_arch_dict.get(
+            softnet_constants.GNOSIS_RA_DICT)
+        kronos_dependency_dict[
+            softnet_constants.KD_COMPONENT_DEPENDENCY_MAP] = component_class_to_package_dict
+        kronos_dependency_dict[
+            softnet_constants.KD_PARENT_TUPLE_LIST] = parent_tuple_list_string
+        kronos_dependency_dict[softnet_constants.KD_EDGE_LIST] = kronos_dependency_edge_list
+        kronos_dependency_dict[
+            softnet_constants.KD_SIMILAR_PACKAGE_MAP] = similar_package_dict
+        kronos_dependency_dict[
+            softnet_constants.KD_PACKAGE_TO_TOPIC_MAP] = package_to_topic_dict
+        _logger.info("Ended Kronos dependency graph generation")
         return KronosDependencyGenerator(kronos_dependency_dict)
 
     def save(self, data_store, filename):
@@ -64,7 +80,8 @@ class KronosDependencyGenerator(object):
         :param file_name: Name of the file that will contain model."""
 
         kronos_dependency_dict = self._dictionary
-        data_store.write_json_file(filename=filename, contents=kronos_dependency_dict)
+        data_store.write_json_file(
+            filename=filename, contents=kronos_dependency_dict)
 
     @classmethod
     def load(cls, data_store, filename):
@@ -80,26 +97,29 @@ class KronosDependencyGenerator(object):
     def _generate_component_class_to_package_edge_list_and_dict(cls, package_list,
                                                                 component_class_list,
                                                                 package_topic_map,):
+        _logger.info("Generating component class to package edge list")
         component_class_to_package_edge_list = list()
         component_class_to_package_dict = dict()
         for package in package_list:
             package_component_class_list = package_topic_map.get(package)
             for component_class in package_component_class_list:
                 edge_dict = dict()
-                edge_dict[EDGE_DICT_FROM] = component_class
-                edge_dict[EDGE_DICT_TO] = package
+                edge_dict[softnet_constants.EDGE_DICT_FROM] = component_class
+                edge_dict[softnet_constants.EDGE_DICT_TO] = package
                 component_class_to_package_edge_list.append(edge_dict)
                 if component_class in component_class_to_package_dict:
-                    temp_package_list = component_class_to_package_dict[component_class]
+                    temp_package_list = component_class_to_package_dict[
+                        component_class]
                     temp_package_list.append(package)
-                    component_class_to_package_dict[component_class] = temp_package_list
+                    component_class_to_package_dict[
+                        component_class] = temp_package_list
                 else:
                     component_class_to_package_dict[component_class] = [package]
 
         for component_class in component_class_list:
             if component_class not in component_class_to_package_dict:
                 component_class_to_package_dict[component_class] = []
-
+        _logger.info("Done generating component class to package edge list")
         return component_class_to_package_edge_list, component_class_to_package_dict
 
     def get_dictionary(self):
@@ -107,7 +127,7 @@ class KronosDependencyGenerator(object):
 
     @classmethod
     def _generate_similar_package_dict(cls, package_to_topic_dict, topic_to_package_dict):
-
+        _logger.info("Started generating similar package dict")
         similar_package_dict = dict()
         for package in package_to_topic_dict:
             topics = package_to_topic_dict[package]
@@ -117,9 +137,10 @@ class KronosDependencyGenerator(object):
                 list_of_package_list.append(topic_package_list)
             distinct_package_list = list(set(sum(list_of_package_list, [])))
             distinct_package_list.remove(package)
-            similar_package_dict_list = get_similar_package_dict_list(
+            similar_package_dict_list = softnet_utils.get_similar_package_dict_list(
                 package=package,
                 package_list=distinct_package_list,
                 package_to_topic_dict=package_to_topic_dict)
             similar_package_dict[package] = similar_package_dict_list
+        _logger.info("Done generating similar package dict")
         return similar_package_dict
