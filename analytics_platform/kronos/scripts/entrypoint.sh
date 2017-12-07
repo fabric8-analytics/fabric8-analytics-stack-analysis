8c48b5f3b6c22d90f13c8116c2aa6bcca2f09d6b
Aagam Shah <aagam@redhat.com>
2017-12-07 17:47:07 +0530

    Remove gevent as worker class
    gevent spans a new process for every worker and joblib doesn't support nested multiprocessing.

--- analytics_platform/kronos/scripts/entrypoint.sh
+++ analytics_platform/kronos/scripts/entrypoint.sh
@@ -7,7 +7,8 @@
 zip -r /tmp/training.zip /analytics_platform /util
 zip -r /tmp/tagging.zip /tagging_platform /util /analytics_platform
 
-gunicorn --pythonpath / -b 0.0.0.0:$SERVICE_PORT --workers=2 -k gevent -t $SERVICE_TIMEOUT rest_api:app
+# WARNING: Don't add worker class as gevent since it doesn't allow PGM to perform scoring in parallel
+gunicorn --pythonpath / -b 0.0.0.0:$SERVICE_PORT --workers=2 -t $SERVICE_TIMEOUT rest_api:app
 
 # --------------------------------------------------------------------------------------------------
 # to make the container alive for indefinite time
