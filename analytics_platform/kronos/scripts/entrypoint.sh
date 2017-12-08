d37a3436e6f78a4553753b5911b6dc7fee541ade
miteshvp <mitpatel@redhat.com>
2017-12-08 15:20:11 +0530

    Performance Benchmarking

--- analytics_platform/kronos/scripts/entrypoint.sh
+++ analytics_platform/kronos/scripts/entrypoint.sh
@@ -8,7 +8,8 @@ zip -r /tmp/training.zip /analytics_platform /util
 zip -r /tmp/tagging.zip /tagging_platform /util /analytics_platform
 
 # WARNING: Don't add worker class as gevent since it doesn't allow PGM to perform scoring in parallel
-gunicorn --pythonpath / -b 0.0.0.0:$SERVICE_PORT --workers=2 -t $SERVICE_TIMEOUT rest_api:app
+gunicorn --pythonpath / -b 0.0.0.0:$SERVICE_PORT --workers=$NUMBER_OF_PROCESSES -t $SERVICE_TIMEOUT
+rest_api:app
 
 # --------------------------------------------------------------------------------------------------
 # to make the container alive for indefinite time
