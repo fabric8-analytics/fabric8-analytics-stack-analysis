b2ae031ba6dd66a7e9f401300eca5a3e7b17ed8e
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-02-19 11:56:25 +0100

    Use different way to specify source files directories

--- tests/scripts/entrypoint-test.sh
+++ tests/scripts/entrypoint-test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python -m pytest -p no:cacheprovider --cov=/analytics_platform:/tagging_platform:/evaluation_platform /tests/unit_tests/
+python -m pytest -p no:cacheprovider --cov=analytics_platform/ --cov=util/ --cov=tagging_platform/ --cov=evaluation_platform/ --cov-report term-missing -vv /tests/unit_tests/
