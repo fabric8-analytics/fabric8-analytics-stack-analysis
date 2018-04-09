38072d15db0c158b65cfbea9ccb331c6610f7b66
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-09 20:13:39 +0200

    Fixed module name

--- tests/unit_tests/test_uranus_deployment_submit_evaluation_job.py
+++ tests/unit_tests/test_uranus_deployment_submit_evaluation_job.py
@@ -2,7 +2,7 @@
 
 import pytest
 
-from evaluation_platform.uranus.src.deployment.submit_evaluation_job import *
+from evaluation_platform.uranus.deployment.submit_evaluation_job import *
 
 if __name__ == '__main__':
     pass
