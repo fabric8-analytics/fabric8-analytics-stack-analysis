46d59c532dc634e49e5456075137fd6dde20090a
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-09 21:03:23 +0200

    Ignore test that is not part of unit test suite

--- tests/scripts/entrypoint-test.sh
+++ tests/scripts/entrypoint-test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python -m pytest -p no:cacheprovider --cov=analytics_platform/ --cov=util/ --cov=tagging_platform/ --cov=evaluation_platform/ --cov-report term-missing -vv /tests/unit_tests/
+python -m pytest -p no:cacheprovider --cov=analytics_platform/ --cov=util/ --cov=tagging_platform/ --cov=evaluation_platform/ --cov-report term-missing --ignore=evaluation_platform/uranus/src/evaluate_data.py -vv /tests/unit_tests/
