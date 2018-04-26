68d085d7ef83347e7d41e005e69b5add06c94e2c
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-26 10:06:26 +0200

    Script to measure cyclomatic complexity, run CC and MI on CI

--- tests/scripts/entrypoint-test.sh
+++ tests/scripts/entrypoint-test.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
+echo "*****************************************"
+echo "*** Cyclomatic complexity measurement ***"
+echo "*****************************************"
+radon cc -s -a -i venv util tagging_platform evaluation_platform
+
+echo "*****************************************"
+echo "*** Maintainability Index measurement ***"
+echo "*****************************************"
+radon mi -s -i venv util tagging_platform evaluation_platform
+
+echo "*****************************************"
+echo "*** Unit tests ***"
+echo "*****************************************"
+
 python -m pytest -p no:cacheprovider --cov=analytics_platform/ --cov=util/ --cov=tagging_platform/ --cov=evaluation_platform/ --cov-report term-missing --ignore=evaluation_platform/uranus/src/evaluate_data.py -vv /tests/unit_tests/
