b861906a1b77354661699fd7e02044094e935dcc
Roman Iuvshyn <riuvshyn@redhat.com>
2018-05-02 19:15:19 +0300

    Introduce RHEL based image

--- cico_setup.sh
+++ cico_setup.sh
@@ -17,6 +17,15 @@ prep() {
 }
 
 build_image() {
+    local push_registry
+    push_registry=$(make get-push-registry)
+    # login before build to be able to pull RHEL parent image
+    if [ -n "${DEVSHIFT_USERNAME}" -a -n "${DEVSHIFT_PASSWORD}" ]; then
+        docker login -u ${DEVSHIFT_USERNAME} -p ${DEVSHIFT_PASSWORD} ${push_registry}
+    else
+        echo "Could not login, missing credentials for the registry"
+        exit 1
+    fi
     # Also runs the recipe for docker-build
     make docker-build-test
 }
@@ -36,15 +45,7 @@ push_image() {
     image_name=$(make get-image-name)
     image_repository=$(make get-image-repository)
     short_commit=$(git rev-parse --short=7 HEAD)
-    push_registry="push.registry.devshift.net"
-
-    # login first
-    if [ -n "${DEVSHIFT_USERNAME}" -a -n "${DEVSHIFT_PASSWORD}" ]; then
-        docker login -u ${DEVSHIFT_USERNAME} -p ${DEVSHIFT_PASSWORD} ${push_registry}
-    else
-        echo "Could not login, missing credentials for the registry"
-        exit 1
-    fi
+    push_registry=$(make get-push-registry)
 
     if [ -n "${ghprbPullId}" ]; then
         # PR build
