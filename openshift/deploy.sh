9217743a38948579d456b71100fd50bd97969c27
Michal Srb <michal@redhat.com>
2018-05-29 10:50:01 +0200

    Introduce OpenShift template with restart policy set to Never
    This new template is for development purposes only. It deploys the
    project as a Pod so restart policy can be set to Never. Standard
    deployment configs only allow "Always" restart policy.
    
    There is also `template-prod.yaml` which is intended for
    staging/production deployments. saas-herder automatically sets restart
    policy to "Always" for those environments.

---
+++ openshift/deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash -e
+
+set -x
+oc whoami
+oc project
+set +x
+
+function oc_process_apply() {
+  echo -e "\n Processing template - $1 ($2) \n"
+  oc process -f $1 $2 | oc apply -f -
+}
+
+here=`dirname $0`
+template="${here}/template.yaml"
+
+oc_process_apply "$template" "-p KRONOS_SCORING_REGION=maven"
