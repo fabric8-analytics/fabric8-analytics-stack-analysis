00816a97bd0a0df01ecd2b449e31908ce3ca7e32
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-27 17:41:16 +0200

    Simple test

--- tests/unit_tests/test_uranus_generate_test_data.py
+++ tests/unit_tests/test_uranus_generate_test_data.py
@@ -1,6 +1,14 @@
 """Tests for the TestData class."""
 
-# from evaluation_platform.uranus.src.generate_test_data import *
+from evaluation_platform.uranus.src.generate_test_data import *
+
+
+def test_test_data_class():
+    """Basic test for the TestData class."""
+    testData = TestData()
+    # there is not any reason the class can't be instancianced
+    assert testData
+
 
 if __name__ == '__main__':
-    pass
+    test_test_data_class()
