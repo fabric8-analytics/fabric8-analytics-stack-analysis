d9b6faa365a8effc710d9a85946f0f79e27cb01d
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-28 10:43:21 +0200

    Fix all remaining common issues

--- tests/unit_tests/test_uranus_generate_test_data.py
+++ tests/unit_tests/test_uranus_generate_test_data.py
@@ -1,6 +1,6 @@
 """Tests for the TestData class."""
 
-from evaluation_platform.uranus.src.generate_test_data import *
+from evaluation_platform.uranus.src.generate_test_data import TestData
 
 
 def test_test_data_class():
